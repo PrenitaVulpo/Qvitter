26cab400264c4ea88ca58079edd9423e34fd9b71
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-06-30 09:37:43 -0300

    add more list items to right drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -51,13 +51,12 @@
       </q-input>
 
       <q-list bordered padding>
-        <q-item>
+        <q-item v-for="headline in headlinesList" :key="headline.labelOverline">
           <q-item-section>
-            <q-item-label overline>OVERLINE</q-item-label>
-            <q-item-label>Single line item</q-item-label>
+            <q-item-label overline>{{ headline.labelOverline }}</q-item-label>
+            <q-item-label>{{ headline.label }}</q-item-label>
             <q-item-label caption>
-              Secondary line text. Lorem ipsum dolor sit amet, consectetur
-              adipiscit elit.
+              {{ headline.caption }}
             </q-item-label>
           </q-item-section>
 
@@ -82,6 +81,33 @@ export default {
     const leftDrawerOpen = ref(false);
     const rightDrawerOpen = ref(false);
 
+    const headlinesList = ref([
+      {
+        labelOverline: 'Sports',
+        label: 'Some team defeated some other team!',
+        caption:
+          'Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit',
+      },
+      {
+        labelOverline: 'Games',
+        label: 'Brand new title!',
+        caption:
+          'Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit',
+      },
+      {
+        labelOverline: 'Tecnology',
+        label: 'Some robot did something!',
+        caption:
+          'Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit',
+      },
+      {
+        labelOverline: 'Science',
+        label: 'covid-20 is  now released!',
+        caption:
+          'Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit',
+      },
+    ]);
+
     return {
       leftDrawerOpen,
       toggleLeftDrawer() {
@@ -92,6 +118,7 @@ export default {
       toggleRightDrawer() {
         rightDrawerOpen.value = !rightDrawerOpen.value;
       },
+      headlinesList,
     };
   },
 };
